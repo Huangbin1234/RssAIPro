735dd726a70dc626cc29abcb4d6070983ec3b995
Huangbin1234 <qianchengluntan@163.com>
2017-07-07 14:37:15 +0800

    启动页面更新

--- app/src/main/java/com/hb/rssai/view/common/LoadActivity.java
+++ app/src/main/java/com/hb/rssai/view/common/LoadActivity.java
@@ -1,17 +1,21 @@
 package com.hb.rssai.view.common;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.hb.rssai.R;
+import com.hb.rssai.view.MainActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoadActivity extends AppCompatActivity {
 
-    @BindView(R.id.sample_text)
+    @BindView(R.id.la_text)
     TextView mSampleText;
 
     @Override
@@ -19,5 +23,30 @@ public class LoadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
         ButterKnife.bind(this);
+        init();
     }
+
+    private void init() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(5 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            handler.sendEmptyMessage(1);
+        });
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    startActivity(new Intent(LoadActivity.this, MainActivity.class));
+                    finish();
+                    break;
+            }
+        }
+    };
 }
