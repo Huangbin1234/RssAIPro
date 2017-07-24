a80e4de73e396d24dad639f281de08e069053b4e
Huangbin1234 <qianchengluntan@163.com>
2017-07-24 15:44:15 +0800

    122121

--- app/src/main/java/com/hb/rssai/view/fragment/SubscriptionFragment.java
+++ app/src/main/java/com/hb/rssai/view/fragment/SubscriptionFragment.java
@@ -2,6 +2,7 @@ package com.hb.rssai.view.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -31,6 +32,7 @@ import com.hb.rssai.bean.UserCollection;
 import com.hb.rssai.constants.Constant;
 import com.hb.rssai.event.RssSourceEvent;
 import com.hb.rssai.util.Base64Util;
+import com.hb.rssai.util.DisplayUtil;
 import com.hb.rssai.util.LiteOrmDBUtil;
 import com.hb.rssai.util.T;
 import com.hb.rssai.view.common.ContentActivity;
@@ -60,6 +62,7 @@ import me.yuqirong.cardswipelayout.CardLayoutManager;
 import me.yuqirong.cardswipelayout.OnSwipeListener;
 
 import static android.app.Activity.RESULT_OK;
+import static com.hb.rssai.app.ProjectApplication.mContext;
 
 public class SubscriptionFragment extends Fragment implements View.OnClickListener, RssSourceAdapter.onItemLongClickedListener {
     private static final String ARG_PARAM1 = "param1";
@@ -139,6 +142,7 @@ private FullyGridLayoutManager mFullyGridLayoutManager;
         mFullyGridLayoutManager = new FullyGridLayoutManager(getContext(), 2);
         mSfRecyclerView.setLayoutManager(mFullyGridLayoutManager);
 //        mSfRecyclerView.addItemDecoration(new DividerGridItemDecoration(getContext()));
+        mSfRecyclerView.addItemDecoration(new SpaceItemDecoration(10));
 
         mSfSwipe.setColorSchemeResources(R.color.refresh_progress_1,
                 R.color.refresh_progress_2, R.color.refresh_progress_3);
@@ -157,6 +161,32 @@ private FullyGridLayoutManager mFullyGridLayoutManager;
 //        });
     }
 
+    public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+        private int space;  //位移间距
+        public SpaceItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            if (parent.getChildAdapterPosition(view) %2 == 0) {
+                outRect.left = 0; //第一列左边贴边
+            } else {
+                if (parent.getChildAdapterPosition(view) %2 == 1) {
+                    outRect.left = space;//第二列移动一个位移间距
+                } else {
+                    outRect.left = space * 2;//由于第二列已经移动了一个间距，所以第三列要移动两个位移间距就能右边贴边，且item间距相等
+                }
+            }
+
+            if (parent.getChildAdapterPosition(view) >= 2) {
+                outRect.top = DisplayUtil.dip2px(getContext(), 10);
+            } else {
+                outRect.top = 0;
+            }
+        }
+
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
