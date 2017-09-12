6d8f8c4d08f3a35121a38badc4e401f189039cbc
Huangbin1234 <qianchengluntan@163.com>
2017-09-13 00:15:35 +0800

    UI virtual keyboard nagbar color is ok

--- app/src/main/java/com/hb/rssai/view/widget/tab/CoordinatorTabLayout.java
+++ app/src/main/java/com/hb/rssai/view/widget/tab/CoordinatorTabLayout.java
@@ -252,11 +252,14 @@ public class CoordinatorTabLayout extends CoordinatorLayout {
         } else {
             mToolbar.setPadding(0, SystemView.getStatusBarHeight(activity) >> 1, 0, 0);
         }
+        Window window = activity.getWindow();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //底部虚拟键盘颜色设置
+            //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.setStatusBarColor(Color.TRANSPARENT);
         } else {
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
