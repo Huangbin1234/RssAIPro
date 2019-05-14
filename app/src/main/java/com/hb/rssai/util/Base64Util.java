08b58cdbd4aa5b101d9a997afdc3703416c63e48
Huangbin1234 <qianchengluntan@163.com>
2019-05-14 22:59:30 +0800

    base64 re

--- app/src/main/java/com/hb/rssai/util/Base64Util.java
+++ app/src/main/java/com/hb/rssai/util/Base64Util.java
@@ -9,20 +9,28 @@ import android.util.Base64;
 public class Base64Util {
     /**
      * Base64编码
+     *
      * @param str
      * @return 编码完成的字符串码
      */
-    public static String getEncodeStr(String str){
-        byte byteArr[]= Base64.encode(str.getBytes(), Base64.DEFAULT);
+    public static String getEncodeStr(String str) {
+        byte byteArr[] = Base64.encode(str.getBytes(), Base64.DEFAULT);
         return new String(byteArr);
     }
+
     /**
      * Base64解码
+     *
      * @param encodeStr
      * @return 解码完成，输出原本字符串
      */
-    public static String getDecodeStr(String encodeStr){
-        byte byteArr[]=Base64.decode(encodeStr, Base64.DEFAULT);
-        return new String (byteArr);
+    public static String getDecodeStr(String encodeStr) {
+        try {
+            byte byteArr[] = Base64.decode(encodeStr, Base64.DEFAULT);
+            return new String(byteArr);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "无法识别分享源：" + encodeStr;
+        }
     }
 }
